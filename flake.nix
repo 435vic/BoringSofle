395d091b7e0c13871b78552af601e04f19f47b39
Victor Quintana <435victorjavier@gmail.com>
2025-08-02 16:07:56 +0000

    add nix shell for ergogen

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    nixpkgs,
+    ...
+  }: let
+    systems = [ "x86_64-linux" "aarch64-linux" ];
+    forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f {
+      pkgs = nixpkgs.legacyPackages.${system};
+    });
+  in {
+    devShells = forAllSystems ({ pkgs }: {
+      default = pkgs.mkShell {
+        packages = [
+          pkgs.ergogen
+        ];
+      };
+    });
+  };
+}
